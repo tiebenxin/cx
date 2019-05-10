1636c934111d449e1d412509b9e5a0537d213b98
shenxin <331334149@qq.com>
2019-05-10 01:10:39 +0000

    token跳转页面

--- app/src/main/java/com/yanlong/im/user/ui/StartPageActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/StartPageActivity.java
@@ -14,13 +14,18 @@ import android.widget.ImageView;
 import com.yanlong.im.MainActivity;
 import com.yanlong.im.R;
 import com.yanlong.im.user.action.UserAction;
+import com.yanlong.im.user.bean.TokenBean;
 
+import net.cb.cb.library.bean.ReturnBean;
 import net.cb.cb.library.utils.SharedPreferencesUtil;
-import net.cb.cb.library.utils.ToastUtil;
 import net.cb.cb.library.view.AppActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class StartPageActivity extends AppActivity {
     private final static long TIME = 2000; //启动页时间
@@ -29,8 +34,8 @@ public class StartPageActivity extends AppActivity {
     private ImageView mBtnStart;
     private ImageView mIvStart;
     private ViewPagerAdapter adapter;
-    private int images [] = {R.mipmap.bg_index1,R.mipmap.bg_index2,R.mipmap.bg_index3};
-    private List <ImageView> imgList;
+    private int images[] = {R.mipmap.bg_index1, R.mipmap.bg_index2, R.mipmap.bg_index3};
+    private List<ImageView> imgList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +53,7 @@ public class StartPageActivity extends AppActivity {
         mIvStart = findViewById(R.id.iv_start);
     }
 
-    private void initViewPager(){
+    private void initViewPager() {
         imgList = new ArrayList<>();
         for (int i = 0; i < images.length; i++) {
             ImageView imageView = new ImageView(StartPageActivity.this);
@@ -66,9 +71,9 @@ public class StartPageActivity extends AppActivity {
 
             @Override
             public void onPageSelected(int i) {
-                if(i == (images.length - 1)){
+                if (i == (images.length - 1)) {
                     mBtnStart.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     mBtnStart.setVisibility(View.GONE);
                 }
             }
@@ -81,7 +86,7 @@ public class StartPageActivity extends AppActivity {
     }
 
 
-    private void startTimer(){
+    private void startTimer() {
         mLayoutGuidance.setVisibility(View.GONE);
         mIvStart.setVisibility(View.VISIBLE);
         Handler handler = new Handler();
@@ -89,45 +94,66 @@ public class StartPageActivity extends AppActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                goActivity();
+                goActivity(false);
             }
         }, TIME);
     }
 
 
-    private void initEvent(){
+    private void initEvent() {
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                goActivity();
+                goActivity(true);
             }
         });
+    }
 
+    private void updateToken(final boolean isFlast) {
+        UserAction userAction = new UserAction();
+        userAction.login4token(new Callback<ReturnBean<TokenBean>>() {
+            @Override
+            public void onResponse(Call<ReturnBean<TokenBean>> call, Response<ReturnBean<TokenBean>> response) {
+                if(isFlast){
+                    startActivity(new Intent(StartPageActivity.this, SelectLoginActivity.class));
+                    onBackPressed();
+                }else{
+                    startActivity(new Intent(StartPageActivity.this, MainActivity.class));
+                    onBackPressed();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ReturnBean<TokenBean>> call, Throwable t) {
+                if (isFlast) {
+                    startActivity(new Intent(StartPageActivity.this, SelectLoginActivity.class));
+                    onBackPressed();
+                }else{
+                    startActivity(new Intent(StartPageActivity.this, PasswordLoginActivity.class));
+                    onBackPressed();
+                }
+            }
+        });
     }
 
 
-    private void goActivity(){
-
-
-        startActivity(new Intent(StartPageActivity.this, MainActivity.class));
-        onBackPressed();
+    private void goActivity(boolean isFlast) {
+        updateToken(isFlast);
     }
 
-    private void showPage(){
+    private void showPage() {
         SharedPreferencesUtil preferencesUtil = new SharedPreferencesUtil(SharedPreferencesUtil.SPName.FIRST_TIME);
         Boolean isFiast = preferencesUtil.get4Json(Boolean.class);
-        if(isFiast == null){
+        if (isFiast == null) {
             initViewPager();
             preferencesUtil.save2Json(true);
-        }else{
+        } else {
             startTimer();
         }
     }
 
 
-
-    class ViewPagerAdapter extends PagerAdapter{
+    class ViewPagerAdapter extends PagerAdapter {
 
         @Override
         public int getCount() {
