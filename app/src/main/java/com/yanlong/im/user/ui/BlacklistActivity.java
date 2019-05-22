df8f5ef79bf4be339266b8e00df52c41ccf373fb
shenxin <331334149@qq.com>
2019-05-22 06:43:06 +0000

    黑名单

---
+++ app/src/main/java/com/yanlong/im/user/ui/BlacklistActivity.java
@@ -0,0 +1,158 @@
+package com.yanlong.im.user.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.mcxtzhang.swipemenulib.SwipeMenuLayout;
+import com.yanlong.im.R;
+import com.yanlong.im.user.action.UserAction;
+import com.yanlong.im.user.bean.UserInfo;
+
+import net.cb.cb.library.bean.ReturnBean;
+import net.cb.cb.library.utils.CallBack;
+import net.cb.cb.library.utils.ToastUtil;
+import net.cb.cb.library.view.ActionbarView;
+import net.cb.cb.library.view.AppActivity;
+import net.cb.cb.library.view.HeadView;
+import net.cb.cb.library.view.MultiListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class BlacklistActivity extends AppActivity {
+
+    private List<UserInfo> blacklist = new ArrayList<>();
+    private UserAction userAction = new UserAction();
+    private HeadView mHeadView;
+    private MultiListView mMtListView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blacklist);
+        initView();
+        initEvent();
+        initData();
+    }
+
+    private void initView() {
+        mHeadView =  findViewById(R.id.headView);
+        mMtListView =  findViewById(R.id.mtListView);
+        mMtListView.init(new BlackAdapter());
+    }
+
+
+    private void initEvent(){
+        mHeadView.getActionbar().setOnListenEvent(new ActionbarView.ListenEvent() {
+            @Override
+            public void onBack() {
+                onBackPressed();
+            }
+
+            @Override
+            public void onRight() {
+
+            }
+        });
+    }
+
+    private void initData() {
+        taskBlacklist();
+    }
+
+
+    private void taskBlacklist() {
+        userAction.friendGet4Black(new CallBack<ReturnBean<List<UserInfo>>>(mMtListView) {
+            @Override
+            public void onResponse(Call<ReturnBean<List<UserInfo>>> call, Response<ReturnBean<List<UserInfo>>> response) {
+                if (response.body() == null) {
+                    return;
+                }
+                if (response.body().isOk()) {
+                    blacklist = response.body().getData();
+                    mMtListView.notifyDataSetChange();
+                }
+
+            }
+        });
+    }
+
+
+    private void taskFriendBlackRemove(Long uid, final int postion) {
+        userAction.friendBlackRemove(uid, new CallBack<ReturnBean>() {
+            @Override
+            public void onResponse(Call<ReturnBean> call, Response<ReturnBean> response) {
+                if (response.body() == null) {
+                    return;
+                }
+                ToastUtil.show(context, response.body().getMsg());
+                if (response.body().isOk()) {
+                    blacklist.remove(postion);
+                    mMtListView.notifyDataSetChange();
+                }
+            }
+        });
+    }
+
+
+    class BlackAdapter extends RecyclerView.Adapter<BlackAdapter.BlackViewHodler> {
+
+
+        @Override
+        public BlackViewHodler onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = inflater.inflate(R.layout.item_blacklist, viewGroup, false);
+            return new BlackViewHodler(view);
+        }
+
+        @Override
+        public void onBindViewHolder(BlackViewHodler viewHolder, final int i) {
+            final UserInfo userInfo = blacklist.get(i);
+            viewHolder.mImgHead.setImageURI(userInfo.getHead() + "");
+            viewHolder.mTxtTime.setText(userInfo.getName());
+            viewHolder.mBtnDel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    taskFriendBlackRemove(userInfo.getUid(), i);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            if (blacklist != null) {
+                return blacklist.size();
+            }
+            return 0;
+        }
+
+
+        class BlackViewHodler extends RecyclerView.ViewHolder {
+            private SwipeMenuLayout mSwipeLayout;
+            private SimpleDraweeView mImgHead;
+            private TextView mTxtTime;
+            private Button mBtnDel;
+
+
+            public BlackViewHodler(@NonNull View itemView) {
+                super(itemView);
+                mSwipeLayout = itemView.findViewById(R.id.swipeLayout);
+                mImgHead = itemView.findViewById(R.id.img_head);
+                mTxtTime = itemView.findViewById(R.id.txt_time);
+                mBtnDel = itemView.findViewById(R.id.btn_del);
+            }
+        }
+
+    }
+
+
+}
