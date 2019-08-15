c22c9e427df1d90c8b6501256ab0362dd9fe6d1c
test4j <245266671@qq.com>
2019-08-15 17:12:13 +0800

    屏蔽转账功能

--- app/src/main/java/com/yanlong/im/chat/ui/ChatActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/ChatActivity.java
@@ -741,8 +741,7 @@ public class ChatActivity extends AppActivity implements ICellEventListener {
 
         if (isGroup()) {//去除群的控件
             viewFunc.removeView(viewAction);
-            viewFunc.removeView(viewTransfer);
-            viewFunc.removeView(viewTransfer);
+            //viewFunc.removeView(viewTransfer);
             viewChatRobot.setVisibility(View.INVISIBLE);
 
 
@@ -752,7 +751,7 @@ public class ChatActivity extends AppActivity implements ICellEventListener {
         }
         viewFunc.removeView(viewRb);
         //test 6.26
-        // viewFunc.removeView(viewTransfer);
+         viewFunc.removeView(viewTransfer);
 
 
 //        mtListView.init(new RecyclerViewAdapter());
