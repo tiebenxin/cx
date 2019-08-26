63fcbdfc60e1790e8b008357d6204c4a20d085ab
Administrator <331334149@qq.com>
2019-08-26 17:06:15 +0800

    密码登录增加忘记密码入口

--- app/src/main/java/com/yanlong/im/user/ui/PasswordLoginActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/PasswordLoginActivity.java
@@ -2,12 +2,8 @@ package com.yanlong.im.user.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.view.AsyncLayoutInflater;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,9 +14,7 @@ import com.yanlong.im.user.action.UserAction;
 import com.yanlong.im.user.bean.TokenBean;
 import com.yanlong.im.utils.PasswordTextWather;
 
-import net.cb.cb.library.AppConfig;
 import net.cb.cb.library.bean.ReturnBean;
-import net.cb.cb.library.utils.CallBack;
 import net.cb.cb.library.utils.CallBack4Btn;
 import net.cb.cb.library.utils.ClickFilter;
 import net.cb.cb.library.utils.InputUtil;
@@ -39,6 +33,7 @@ public class PasswordLoginActivity extends AppActivity implements View.OnClickLi
     private TextView mTvIdentifyingCode;
     private Button mBtnLogin;
     private UserAction userAction = new UserAction();
+    private TextView tvForgetPassword;
 
 
     @Override
@@ -49,22 +44,18 @@ public class PasswordLoginActivity extends AppActivity implements View.OnClickLi
         initEvent();
     }
 
-    private void initView(){
-        mHeadView =  findViewById(R.id.headView);
+    private void initView() {
+        mHeadView = findViewById(R.id.headView);
         mEtPhoneContent = findViewById(R.id.et_phone_content);
-        mEtPasswordContent =  findViewById(R.id.et_password_content);
-        mTvIdentifyingCode =  findViewById(R.id.tv_identifying_code);
-        mBtnLogin =  findViewById(R.id.btn_login);
+        mEtPasswordContent = findViewById(R.id.et_password_content);
+        mTvIdentifyingCode = findViewById(R.id.tv_identifying_code);
+        mBtnLogin = findViewById(R.id.btn_login);
         mHeadView.getActionbar().setTxtRight("注册");
-       /* if(AppConfig.DEBUG){
-            mEtPhoneContent.setText("13000000000");
-            mEtPasswordContent.setText("123456");
-        }*/
-
+        tvForgetPassword = findViewById(R.id.tv_forget_password);
     }
 
-    private void initEvent(){
-        mEtPasswordContent.addTextChangedListener(new PasswordTextWather(mEtPasswordContent,this));
+    private void initEvent() {
+        mEtPasswordContent.addTextChangedListener(new PasswordTextWather(mEtPasswordContent, this));
         mHeadView.getActionbar().setOnListenEvent(new ActionbarView.ListenEvent() {
             @Override
             public void onBack() {
@@ -73,11 +64,19 @@ public class PasswordLoginActivity extends AppActivity implements View.OnClickLi
 
             @Override
             public void onRight() {
-                Intent intent = new Intent(PasswordLoginActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(PasswordLoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
         mTvIdentifyingCode.setOnClickListener(this);
+        ClickFilter.onClick(tvForgetPassword, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forgotPasswordIntent = new Intent(PasswordLoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(forgotPasswordIntent);
+            }
+        });
+
         ClickFilter.onClick(mBtnLogin, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -90,9 +89,9 @@ public class PasswordLoginActivity extends AppActivity implements View.OnClickLi
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.tv_identifying_code:
-                Intent intent = new Intent(this,IdentifyingCodeActivity.class);
+                Intent intent = new Intent(this, IdentifyingCodeActivity.class);
                 startActivity(intent);
                 break;
         }
@@ -100,31 +99,31 @@ public class PasswordLoginActivity extends AppActivity implements View.OnClickLi
     }
 
 
-    private void login(){
+    private void login() {
         String phone = mEtPhoneContent.getText().toString();
         String password = mEtPasswordContent.getText().toString();
-        if(TextUtils.isEmpty(phone)){
-            ToastUtil.show(this,"请输入账号");
+        if (TextUtils.isEmpty(phone)) {
+            ToastUtil.show(this, "请输入账号");
             return;
         }
-        if(TextUtils.isEmpty(password)){
-            ToastUtil.show(this,"请输入密码");
+        if (TextUtils.isEmpty(password)) {
+            ToastUtil.show(this, "请输入密码");
             return;
         }
         userAction.login(phone, password, UserAction.getDevId(this), new CallBack4Btn<ReturnBean<TokenBean>>(mBtnLogin) {
 
             @Override
             public void onResp(Call<ReturnBean<TokenBean>> call, Response<ReturnBean<TokenBean>> response) {
-                if(response.body() == null){
-                    ToastUtil.show(context,"登录异常");
+                if (response.body() == null) {
+                    ToastUtil.show(context, "登录异常");
                     return;
                 }
-                if(response.body().isOk()){
-                    Intent intent = new Intent(getContext(),MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                if (response.body().isOk()) {
+                    Intent intent = new Intent(getContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
-                }else{
-                    ToastUtil.show(getContext(),response.body().getMsg());
+                } else {
+                    ToastUtil.show(getContext(), response.body().getMsg());
                 }
             }
         });
