8a0bbc3dbb1bbcdfb0e1122058f522fc42e3b106
test4j <245266671@qq.com>
2019-09-17 10:53:01 +0800

    进入聊天就修改会话阅读数量

--- app/src/main/java/com/yanlong/im/chat/ui/ChatActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/ChatActivity.java
@@ -954,6 +954,9 @@ public class ChatActivity extends AppActivity implements ICellEventListener {
         });
 
 
+        //9.17 进去后就清理会话的阅读数量
+        taskCleanRead();
+        EventBus.getDefault().post(new EventRefreshMainMsg());
     }
 
     private void uploadVoice(String file, final MsgAllBean bean) {
@@ -1219,6 +1222,7 @@ public class ChatActivity extends AppActivity implements ICellEventListener {
     @Override
     protected void onDestroy() {
         taskDarftSet();
+        EventBus.getDefault().post(new EventRefreshMainMsg());
         //取消监听
         SocketUtil.getSocketUtil().removeEvent(msgEvent);
         EventBus.getDefault().unregister(this);
@@ -2547,7 +2551,7 @@ public class ChatActivity extends AppActivity implements ICellEventListener {
     private void taskDarftSet() {
         String df = edtChat.getText().toString();
         dao.sessionDraft(toGid, toUId, df);
-        EventBus.getDefault().post(new EventRefreshMainMsg());
+
 
     }
 
