59b24a8e5dc3bf9c7b0534c1ee13a31669382906
Administrator <331334149@qq.com>
2019-09-17 15:28:58 +0800

    优化版本对比

--- appcore/src/main/java/net/cb/cb/library/utils/VersionUtil.java
+++ appcore/src/main/java/net/cb/cb/library/utils/VersionUtil.java
@@ -2,6 +2,11 @@ package net.cb.cb.library.utils;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class VersionUtil {
     /**
@@ -40,8 +45,36 @@ public class VersionUtil {
     }
 
 
-    public static String getPhoneModel(){
-        return android.os.Build.BRAND +" "+ android.os.Build.MODEL;
+    /**
+     * 比较新版本
+     */
+    public static boolean isNewVersion(Context context, String newVerName) {
+        List<String> newVerNames ;
+        List<String> OldVerNames ;
+        if (TextUtils.isEmpty(newVerName)) {
+            return false;
+        } else {
+            newVerNames = Arrays.asList(newVerName.split("\\."));
+        }
+        String OldVerName = getVerName(context);
+        OldVerNames = Arrays.asList(OldVerName.split("\\."));
+
+        if ((newVerNames == null || newVerNames.size() != 3) || (OldVerNames == null || OldVerNames.size() != 3)) {
+            return false;
+        } else {
+            int newVerNum = Integer.valueOf(newVerNames.get(0)) * 100 + Integer.valueOf(newVerNames.get(1)) * 10 + Integer.valueOf(newVerNames.get(2));
+            int oldVerNum = Integer.valueOf(OldVerNames.get(0)) * 100 + Integer.valueOf(OldVerNames.get(1)) * 10 + Integer.valueOf(OldVerNames.get(2));
+            if (oldVerNum < newVerNum) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
+    public static String getPhoneModel() {
+        return android.os.Build.BRAND + " " + android.os.Build.MODEL;
     }
 
 }
