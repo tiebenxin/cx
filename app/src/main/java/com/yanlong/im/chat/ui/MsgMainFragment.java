1fb55713b3a7c70851c42891a68e5ec17210f9e5
wl <1062678603@qq.com>
2019-09-29 19:27:36 +0800

    修改msg列表页面刷新

--- app/src/main/java/com/yanlong/im/chat/ui/MsgMainFragment.java
+++ app/src/main/java/com/yanlong/im/chat/ui/MsgMainFragment.java
@@ -465,7 +465,7 @@ public class MsgMainFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        taskListData();
+//        taskListData();
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
