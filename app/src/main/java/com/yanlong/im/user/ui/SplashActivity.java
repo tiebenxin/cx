a4a1d54033dbf4fb21556b78cb2d38bb86ab6b9a
wl <1062678603@qq.com>
2019-11-09 17:20:07 +0800

    修复splash页面跳动问题

--- app/src/main/java/com/yanlong/im/user/ui/SplashActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/SplashActivity.java
@@ -2,6 +2,7 @@ package com.yanlong.im.user.ui;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -59,8 +60,8 @@ public class SplashActivity extends AppActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         //6.27 如果已经启动则不在启动这个页面
         if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
@@ -69,11 +70,25 @@ public class SplashActivity extends AppActivity {
         }
 
         setContentView(R.layout.activity_start_page);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        hideNavigarionBar();
         initView();
         initEvent();
         showPage();
 
+    }
+
+    private void hideNavigarionBar() {
+        if (Build.VERSION.SDK_INT < 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.GONE);
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        }
     }
 
 
@@ -201,8 +216,8 @@ public class SplashActivity extends AppActivity {
 
     // 提前将全屏切换为非全屏状态，解决从全屏进入非全屏标题栏闪动的问题
     private void setFinishFlags() {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
     }
 
 
