4fa3f80d2eeec2e0548573266629c675e7d16db4
ZhuJiaYi <zhujiayi1234@qq.com>
2019-11-13 19:37:35 +0800

    版本更新UI改版 100% 进度条定制view 20%

--- app/src/main/java/com/yanlong/im/utils/update/UpdateAppDialog.java
+++ app/src/main/java/com/yanlong/im/utils/update/UpdateAppDialog.java
@@ -4,6 +4,7 @@ package com.yanlong.im.utils.update;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -21,32 +22,38 @@ public class UpdateAppDialog {
     private Event event;
 
     private Context context;
-    private TextView txtAlertTitle;
     private TextView txtAlertMsg;
+    private TextView tvVersionNumber;
     private LinearLayout viewNo;
     private Button btnCl;
     private Button btnUpdate;
     private ProgressBar mProgressNum;
     private Button btnInstall;
-    private String title;
+    private String newVersionNum;//发现新版本号
 
     //自动寻找控件
     private void findViews(View rootview) {
-        txtAlertTitle = rootview.findViewById(R.id.txt_alert_title);
         txtAlertMsg = rootview.findViewById(R.id.txt_alert_msg);
         viewNo = rootview.findViewById(R.id.view_no);
         btnCl = rootview.findViewById(R.id.btn_cl);
         btnUpdate = rootview.findViewById(R.id.btn_update);
         mProgressNum =  rootview.findViewById(R.id.progress_num);
         btnInstall = rootview.findViewById(R.id.btn_install);
+        tvVersionNumber = rootview.findViewById(R.id.tv_version_number);
     }
 
 
     //自动生成的控件事件
-    private void initEvent(String title, String msg) {
-        this.title = title;
-        txtAlertTitle.setText(title);
+    private void initEvent(String newVersionNum, String msg) {
+        this.newVersionNum = newVersionNum;
         txtAlertMsg.setText(msg);
+        //显示绿色版本号
+        if(!TextUtils.isEmpty(newVersionNum)){
+            tvVersionNumber.setVisibility(View.VISIBLE);
+            tvVersionNumber.setText("V"+newVersionNum);
+        }else {
+            tvVersionNumber.setVisibility(View.INVISIBLE);
+        }
         btnCl.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 event.onON();
@@ -74,7 +81,7 @@ public class UpdateAppDialog {
         alertDialog.dismiss();
     }
 
-    public void init(Activity activity, String title, String msg, Event e) {
+    public void init(Activity activity, String newVersionNum, String msg, Event e) {
         event = e;
         this.context = activity;
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
@@ -86,35 +93,37 @@ public class UpdateAppDialog {
 
         alertDialog.setView(rootView);
         findViews(rootView);
-        initEvent(title, msg);
+        initEvent(newVersionNum, msg);
 
     }
 
 
     public void updateStart(){
-        txtAlertTitle.setText("开始下载");
-        txtAlertMsg.setVisibility(View.GONE);
         mProgressNum.setVisibility(View.VISIBLE);
+        btnUpdate.setVisibility(View.GONE);
         btnUpdate.setClickable(false);
+        btnCl.setBackgroundResource(R.drawable.shape_18radius_0fa6ea);
+        btnCl.setTextColor(context.getResources().getColor(R.color.white));
     }
 
 
     public void updateStop(){
-        txtAlertTitle.setText(title+"");
-        txtAlertMsg.setVisibility(View.VISIBLE);
         mProgressNum.setVisibility(View.GONE);
         btnUpdate.setClickable(true);
     }
 
 
     public void downloadComplete(){
-        txtAlertTitle.setText("下载完成");
+//        txtAlertTitle.setText("下载完成");
         btnCl.setText("取消");
-        txtAlertMsg.setText("是否安装");
+        btnCl.setBackgroundResource(R.color.white);
+        btnCl.setTextColor(context.getResources().getColor(R.color.c_969696));
+        txtAlertMsg.setText("下载完成，是否安装?");
         txtAlertMsg.setVisibility(View.VISIBLE);
         mProgressNum.setVisibility(View.GONE);
         btnUpdate.setVisibility(View.GONE);
         btnInstall.setVisibility(View.VISIBLE);
+
     }
 
 
@@ -132,8 +141,10 @@ public class UpdateAppDialog {
     public void show() {
         alertDialog.show();
         WindowManager.LayoutParams p = alertDialog.getWindow().getAttributes();
-        p.width = DensityUtil.dip2px(context, 300);
+        p.width = DensityUtil.dip2px(context, 230);
+        p.height = DensityUtil.dip2px(context, 363);
         alertDialog.getWindow().setAttributes(p);
+        alertDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
     }
 
 
