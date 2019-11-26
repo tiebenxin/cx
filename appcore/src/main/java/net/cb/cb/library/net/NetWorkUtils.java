ef7610d94abf241696d104d4db87c7ff4dccce88
wl <1062678603@qq.com>
2019-11-26 14:51:56 +0800

    修复接收离线可能崩溃问题

---
+++ appcore/src/main/java/net/cb/cb/library/net/NetWorkUtils.java
@@ -0,0 +1,64 @@
+package net.cb.cb.library.net;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import net.cb.cb.library.AppConfig;
+
+/**
+ * @anthor Liszt
+ * @data 2019/11/26
+ * Description
+ */
+public class NetWorkUtils {
+    /***
+     * 网络连接检测
+     *
+     * @return
+     */
+    public static boolean isNetworkConnected() {
+        Context context = AppConfig.APP_CONTEXT;
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return info != null && info.isConnected();
+    }
+
+    /**
+     * 获取当前ip地址
+     *
+     * @param context
+     * @return
+     */
+    public static String getLocalIpAddress(Context context) {
+        try {
+            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            int i = wifiInfo.getIpAddress();
+            return int2ip(i);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 将ip的整数形式转换成ip形式
+     *
+     * @param ipInt
+     * @return
+     */
+    public static String int2ip(int ipInt) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ipInt & 0xFF).append(".");
+        sb.append((ipInt >> 8) & 0xFF).append(".");
+        sb.append((ipInt >> 16) & 0xFF).append(".");
+        sb.append((ipInt >> 24) & 0xFF);
+        return sb.toString();
+    }
+
+
+}
