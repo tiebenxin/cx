c84fd813603d1d3a508018856d028c7d52d1b084
ZhuJiaYi <zhujiayi1234@qq.com>
2019-11-29 09:03:55 +0800

    Revert "[零钱模块] 零钱首页UI界面补充调整"
    This reverts commit 2cd3c674

--- cxpay/src/main/java/com/hm/cxpay/ui/LooseChangeActivity.java
+++ cxpay/src/main/java/com/hm/cxpay/ui/LooseChangeActivity.java
@@ -1,41 +1,79 @@
 package com.hm.cxpay.ui;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.hm.cxpay.R;
-import com.hm.cxpay.base.BasePayActivity;
 import com.hm.cxpay.controller.ControllerPaySetting;
-import com.hm.cxpay.databinding.ActivityLooseChangeBinding;
 import com.hm.cxpay.net.FGObserver;
 import com.hm.cxpay.net.PayHttpUtils;
 import com.hm.cxpay.rx.RxSchedulers;
 import com.hm.cxpay.rx.data.BaseResponse;
 
+import net.cb.cb.library.view.ActionbarView;
+import net.cb.cb.library.view.AppActivity;
+import net.cb.cb.library.view.HeadView;
+
 /*
  * 零钱首页
  * */
-public class LooseChangeActivity extends BasePayActivity {
+public class LooseChangeActivity extends AppActivity {
 
-    private ActivityLooseChangeBinding ui;
     private ControllerPaySetting viewSettingOfPsw;
     private ControllerPaySetting viewRecordOfTransaction;
     private ControllerPaySetting viewMyCard;
     private ControllerPaySetting viewMyRedEnvelope;
 
+    private HeadView mHeadView;
+    private TextView tvMoney;//余额
+    private Button btnRecharge;//充值
+    private Button btnWithdrawDeposit;//提现
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ui = DataBindingUtil.setContentView(this, R.layout.activity_loose_change);
         setContentView(R.layout.activity_loose_change);
-        initToolBar(ui.viewTitleBar, true);
-        ui.viewTitleBar.setTitleText("零钱");
         initView();
         initEvent();
     }
 
     private void initView() {
+        mHeadView = findViewById(R.id.headView);
+        tvMoney = findViewById(R.id.tv_money);
+        btnRecharge = findViewById(R.id.btn_recharge);
+        btnWithdrawDeposit = findViewById(R.id.btn_withdraw_deposit);
+    }
+
+
+    private void initEvent() {
+        //标题栏事件
+        mHeadView.getActionbar().setOnListenEvent(new ActionbarView.ListenEvent() {
+            @Override
+            public void onBack() {
+                onBackPressed();
+            }
+
+            @Override
+            public void onRight() {
+
+            }
+        });
+        //充值
+        btnRecharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //提现
+        btnWithdrawDeposit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         //红包明细
         viewMyRedEnvelope = new ControllerPaySetting(findViewById(R.id.viewMyRedEnvelope));
         viewMyRedEnvelope.init(R.mipmap.ic_red_packet_info, R.string.my_red_envelope, "");
@@ -51,7 +89,7 @@ public class LooseChangeActivity extends BasePayActivity {
         viewAccountInfo.setOnClickListener(new ControllerPaySetting.OnControllerClickListener() {
             @Override
             public void onClick() {
-                authUser("400100192201012233", "李白");
+//                authUser("400100192201012233", "李白");
             }
         });
         //我的银行卡
@@ -79,27 +117,6 @@ public class LooseChangeActivity extends BasePayActivity {
         viewSettingOfPsw.setOnClickListener(new ControllerPaySetting.OnControllerClickListener() {
             @Override
             public void onClick() {
-
-            }
-        });
-
-
-    }
-
-
-    private void initEvent() {
-        //充值
-        ui.btnRecharge.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        //提现
-        ui.btnWithdrawDeposit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
             }
         });
