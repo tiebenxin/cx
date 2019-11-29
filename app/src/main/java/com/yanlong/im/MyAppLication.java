3e783f90c6dcdc28a05c29242e4d55d7690946d2
549520089@qq.com <549520089@qq.com>
2019-11-29 15:12:03 +0800

    无

--- app/src/main/java/com/yanlong/im/MyAppLication.java
+++ app/src/main/java/com/yanlong/im/MyAppLication.java
@@ -86,7 +86,7 @@ public class MyAppLication extends MainApplication {
                 AppConfig.UP_PATH = "development";
                 break;
             case "release"://正式服
-                AppConfig.DEBUG = true; // false true
+                AppConfig.DEBUG = false; // false true
                 //---------------------------
                 AppConfig.SOCKET_IP = "im-app.zhixun6.com";
                 AppConfig.URL_HOST = "https://" + AppConfig.SOCKET_IP + ":8080";
