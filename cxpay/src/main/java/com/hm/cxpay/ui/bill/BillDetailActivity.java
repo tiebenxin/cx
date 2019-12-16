929bb6199be3b22b0de28c4c04c564097562afe7
ZhuJiaYi <zhujiayi1234@qq.com>
2019-12-16 16:48:09 +0800

    [优化] 零钱详情分转换为元显示 账单详情提现状态显示异常

--- cxpay/src/main/java/com/hm/cxpay/ui/bill/BillDetailActivity.java
+++ cxpay/src/main/java/com/hm/cxpay/ui/bill/BillDetailActivity.java
@@ -318,8 +318,21 @@ public class BillDetailActivity extends AppActivity {
                 tvWithdrawStatusThree.setText("到账\n"+DateUtils.timeStamp2Date(data.getStatConfirmTime(), ""));
                 tvWithdrawStatusThree.setVisibility(View.VISIBLE);
                 ivWithdrawFinished.setVisibility(View.VISIBLE);
-            } else {
-                tvWithdrawStatusTwo.setText("银行处理中");
+            }else if(data.getStat()==2){
+                tvWithdrawStatusTwo.setText("已部分退款");
+                ivWithdrawStatusTwo.setVisibility(View.VISIBLE);
+                tvWithdrawStatusThree.setVisibility(View.GONE);
+                ivWithdrawFinished.setVisibility(View.GONE);
+            }else if(data.getStat()==99){
+                tvWithdrawStatusTwo.setText("处理中");
+                ivWithdrawStatusTwo.setVisibility(View.VISIBLE);
+                tvWithdrawStatusThree.setVisibility(View.GONE);
+                ivWithdrawFinished.setVisibility(View.GONE);
+            }else if(data.getStat()==200){
+                tvWithdrawStatusTwo.setText("已全额退款");
+                ivWithdrawStatusTwo.setVisibility(View.VISIBLE);
+                tvWithdrawStatusThree.setVisibility(View.GONE);
+                ivWithdrawFinished.setVisibility(View.GONE);
             }
             tvWithdrawMoney.setText("¥"+UIUtils.getYuan(data.getAmt())+"元");
             tvWithdrawCreateTime.setText(DateUtils.timeStamp2Date(data.getCreateTime(), ""));
