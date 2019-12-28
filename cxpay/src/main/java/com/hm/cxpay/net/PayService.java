4e405058e3898114b278ccbbdd8a8c9c0941ab31
ZhuJiaYi <zhujiayi1234@qq.com>
2019-12-28 14:00:30 +0800

    修复-添加签名导致身份认证崩溃问题

--- cxpay/src/main/java/com/hm/cxpay/net/PayService.java
+++ cxpay/src/main/java/com/hm/cxpay/net/PayService.java
@@ -36,155 +36,125 @@ import retrofit2.http.QueryMap;
 public interface PayService {
 
     //用户认证接口,idNumber 身份证号码；idType 目前只有身份证，传1即可；realName 真实姓名
-    @FormUrlEncoded
     @POST(Route.URL_USER_AUTH)
-    Observable<BaseResponse> authUserInfo(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> authUserInfo(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取用户信息接口
-    @FormUrlEncoded
     @POST(Route.GET_USER_INFO)
-    Observable<BaseResponse<UserBean>> getUserInfo(@FieldMap Map<String, String> map);
+    Observable<BaseResponse<UserBean>> getUserInfo(@QueryMap Map<String, String> map);
 
     //银行卡检测接口
-    @FormUrlEncoded
     @POST(Route.CHECK_BANK_CARD)
-    Observable<BaseResponse<BankInfo>> checkBankCard(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<BankInfo>> checkBankCard(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //申请绑定银行卡
-    @FormUrlEncoded
     @POST(Route.APPLY_BIND_BANK_CARD)
-    Observable<BaseResponse<BindBankInfo>> applyBindBankCard(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<BindBankInfo>> applyBindBankCard(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取绑定银行卡列表
-    @FormUrlEncoded
     @POST(Route.GET_BIND_BANK_CARD)
-    Observable<BaseResponse<List<BankBean>>> getBindBankCardList(@FieldMap Map<String, String> map);
+    Observable<BaseResponse<List<BankBean>>> getBindBankCardList(@QueryMap Map<String, String> map);
 
     //绑定银行卡
-    @FormUrlEncoded
     @POST(Route.BIND_BANK)
-    Observable<BaseResponse> bindBank(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> bindBank(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //设置支付密码
-    @FormUrlEncoded
     @POST(Route.SET_PAYWORD)
-    Observable<BaseResponse> setPayword(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> setPayword(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //修改支付密码
-    @FormUrlEncoded
     @POST(Route.MODIFY_PAYWORD)
-    Observable<BaseResponse> modifyPayword(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> modifyPayword(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //设置检查密码
-    @FormUrlEncoded
     @POST(Route.CHECK_PAYWORD)
-    Observable<BaseResponse> checkPayword(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> checkPayword(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //解绑银行卡
-    @FormUrlEncoded
     @POST(Route.UNBIND_BANK_CARD)
-    Observable<BaseResponse> deleteBankcard(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> deleteBankcard(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //充值
-    @FormUrlEncoded
     @POST(Route.TO_RECHARGE)
-    Observable<BaseResponse<CommonBean>> toRecharge(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> toRecharge(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //提现
-    @FormUrlEncoded
     @POST(Route.TO_WITHDRAW)
-    Observable<BaseResponse<CommonBean>> toWithdraw(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> toWithdraw(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取系统费率
-    @FormUrlEncoded
     @POST(Route.GET_RATE)
-    Observable<BaseResponse<CommonBean>> getRate(@FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> getRate(@QueryMap Map<String, String> map);
 
     //绑定手机-获取验证码
-    @FormUrlEncoded
     @POST(Route.GET_PHONE_CODE)
-    Observable<BaseResponse> getCode(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> getCode(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //绑定手机-获取当前用户IM手机号
-    @FormUrlEncoded
     @POST(Route.GET_MY_PHONE)
-    Observable<BaseResponse<CommonBean>> getMyPhone(@FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> getMyPhone(@QueryMap Map<String, String> map);
 
     //绑定手机号
     @POST(Route.BIND_PHONE)
-    @FormUrlEncoded
-    Observable<BaseResponse> bindPhoneNum(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse> bindPhoneNum(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取账单明细
     @POST(Route.GET_BILL_DETAILS_LIST)
-    @FormUrlEncoded
-    Observable<BaseResponse<BillBean>> getBillDetailsList(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<BillBean>> getBillDetailsList(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取零钱明细
-    @FormUrlEncoded
     @POST(Route.GET_CHANGE_DETAILS_LIST)
-    Observable<BaseResponse<BillBean>> getChangeDetailsList(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<BillBean>> getChangeDetailsList(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //验证实名信息-忘记密码辅助验证第一步
-    @FormUrlEncoded
     @POST(Route.CHECK_REALNAME_INFO)
-    Observable<BaseResponse<CommonBean>> checkRealNameInfo(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> checkRealNameInfo(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //绑定银行卡-忘记密码辅助验证第三步
-    @FormUrlEncoded
     @POST(Route.BIND_BANK_CARD)
-    Observable<BaseResponse<CommonBean>> bindBankCard(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> bindBankCard(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //验证短信验证码-忘记密码辅助验证第四步
-    @FormUrlEncoded
     @POST(Route.CHECK_CODE)
-    Observable<BaseResponse<CommonBean>> checkCode(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<CommonBean>> checkCode(@Body RequestBody body, @QueryMap Map<String, String> map);
 
 
     //获取红包明细
-    @FormUrlEncoded
     @POST(Route.GET_RED_ENVELOPE_DETAILS)
-    Observable<BaseResponse<RedDetailsBean>> getRedEnvelopeDetails(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<RedDetailsBean>> getRedEnvelopeDetails(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //发红包
-    @FormUrlEncoded
     @POST(Route.SEND_RED_ENVELOPE)
-    Observable<BaseResponse<SendResultBean>> sendRedEnvelope(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<SendResultBean>> sendRedEnvelope(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //抢红包
-    @FormUrlEncoded
     @POST(Route.SNATCH_RED_ENVELOPE)
-    Observable<BaseResponse<GrabEnvelopeBean>> grabRedEnvelope(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<GrabEnvelopeBean>> grabRedEnvelope(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //拆红包
-    @FormUrlEncoded
     @POST(Route.OPEN_RED_ENVELOPE)
-    Observable<BaseResponse<OpenEnvelopeBean>> openRedEnvelope(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<OpenEnvelopeBean>> openRedEnvelope(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //查看红包记录详情
-    @FormUrlEncoded
     @POST(Route.GET_RED_ENVELOPE_DETAIL)
-    Observable<BaseResponse<EnvelopeDetailBean>> getEnvelopeDetail(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<EnvelopeDetailBean>> getEnvelopeDetail(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //发送转账
-    @FormUrlEncoded
     @POST(Route.SEND_TRANSFER)
-    Observable<BaseResponse<SendResultBean>> sendTransfer(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<SendResultBean>> sendTransfer(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //获取转账详情
-    @FormUrlEncoded
     @POST(Route.GET_TRANSFER_DETAIL)
-    Observable<BaseResponse<TransferDetailBean>> getTransferDetail(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<TransferDetailBean>> getTransferDetail(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //领取转账
-    @FormUrlEncoded
     @POST(Route.RECEIVE_TRANSFER)
-    Observable<BaseResponse<TransferResultBean>> receiveTransfer(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<TransferResultBean>> receiveTransfer(@Body RequestBody body, @QueryMap Map<String, String> map);
 
     //拒收转账
-    @FormUrlEncoded
     @POST(Route.RETURN_TRANSFER)
-    Observable<BaseResponse<TransferResultBean>> returnTransfer(@Body RequestBody body, @FieldMap Map<String, String> map);
+    Observable<BaseResponse<TransferResultBean>> returnTransfer(@Body RequestBody body, @QueryMap Map<String, String> map);
 
 
 }
