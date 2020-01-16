3872089d130875f3eb19eff0b9f10f55870a3514
ZhuJiaYi <zhujiayi1234@qq.com>
2020-01-16 20:59:29 +0800

    预发布环境改为测试环境

--- app/src/main/java/com/yanlong/im/MyAppLication.java
+++ app/src/main/java/com/yanlong/im/MyAppLication.java
@@ -75,16 +75,16 @@ public class MyAppLication extends MainApplication {
                 AppConfig.DEBUG = true;
                 //---------------------------
 //                AppConfig.SOCKET_IP = "beta.zhixun6.com";
-//                AppConfig.SOCKET_IP = "yanlong.1616d.top";
-//                AppConfig.URL_HOST = "https://" + AppConfig.SOCKET_IP + ":8080";
-//                AppConfig.SOCKET_PORT = 19991;
-//                AppConfig.UP_PATH = "test-environment";
-
-                AppConfig.SOCKET_IP = "beta.zhixun6.com";
-                AppConfig.SOCKET_IP = "im-app.zhixun6.com";
+                AppConfig.SOCKET_IP = "yanlong.1616d.top";
                 AppConfig.URL_HOST = "https://" + AppConfig.SOCKET_IP + ":8080";
                 AppConfig.SOCKET_PORT = 19991;
-                AppConfig.UP_PATH = "product-environment";
+                AppConfig.UP_PATH = "test-environment";
+
+//                AppConfig.SOCKET_IP = "beta.zhixun6.com";
+//                AppConfig.SOCKET_IP = "im-app.zhixun6.com";
+//                AppConfig.URL_HOST = "https://" + AppConfig.SOCKET_IP + ":8080";
+//                AppConfig.SOCKET_PORT = 19991;
+//                AppConfig.UP_PATH = "product-environment";
                 break;
             case "pre": //预发布服  美国 usa-test.1616d.top    香港 hk-test.1616d.top
                 AppConfig.DEBUG = false;
