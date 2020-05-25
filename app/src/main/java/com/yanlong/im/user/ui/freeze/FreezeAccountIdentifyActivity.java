2b9a83e6b8101e61c632ebfad40094924784cbb2
wl <1062678603@qq.com>
2020-05-25 17:39:58 +0800

    冻结账号UI

---
+++ app/src/main/java/com/yanlong/im/user/ui/freeze/FreezeAccountIdentifyActivity.java
@@ -0,0 +1,91 @@
+package com.yanlong.im.user.ui.freeze;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.yanlong.im.R;
+import com.yanlong.im.databinding.ActivityFreezeIdentifyBinding;
+import com.yanlong.im.user.action.UserAction;
+import com.yanlong.im.user.ui.IdentifyingCodeActivity;
+
+import net.cb.cb.library.bean.ReturnBean;
+import net.cb.cb.library.utils.CallBack;
+import net.cb.cb.library.utils.CheckUtil;
+import net.cb.cb.library.utils.CountDownUtil;
+import net.cb.cb.library.utils.ToastUtil;
+import net.cb.cb.library.view.ActionbarView;
+import net.cb.cb.library.view.AppActivity;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+/**
+ * @author Liszt
+ * @date 2020/5/25
+ * Description 冻结账号-手机验证界面
+ */
+public class FreezeAccountIdentifyActivity extends AppActivity {
+
+    private ActivityFreezeIdentifyBinding ui;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ui = DataBindingUtil.setContentView(this, R.layout.activity_freeze_identify);
+        ui.headView.getActionbar().setOnListenEvent(new ActionbarView.ListenEvent() {
+            @Override
+            public void onBack() {
+                onBackPressed();
+            }
+
+            @Override
+            public void onRight() {
+
+            }
+        });
+
+        ui.tvGetVerificationCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initCountDownUtil();
+            }
+        });
+    }
+
+    private void initCountDownUtil() {
+        final String phone = ui.etPhoneContent.getText().toString();
+        if (TextUtils.isEmpty(phone)) {
+            ToastUtil.show(FreezeAccountIdentifyActivity.this, "请填写手机号码");
+            return;
+        }
+        if (!CheckUtil.isMobileNO(phone)) {
+            ToastUtil.show(this, "手机号格式不正确");
+            return;
+        }
+        CountDownUtil.getTimer(60, ui.tvGetVerificationCode, "发送验证码", this, new CountDownUtil.CallTask() {
+            @Override
+            public void task() {
+                taskGetSms(phone);
+            }
+        });
+    }
+
+    //TODO:服务端需要重新添加businessType
+    private void taskGetSms(String phone) {
+        new UserAction().smsCaptchaGet(phone, "xxxx", new CallBack<ReturnBean>() {
+            @Override
+            public void onResponse(Call<ReturnBean> call, Response<ReturnBean> response) {
+                if (response.body() == null) {
+                    ToastUtil.show(context, "冻结失败");
+                    return;
+                }
+                if (response.body() == null) {
+                    return;
+                }
+                ToastUtil.show(FreezeAccountIdentifyActivity.this, response.body().getMsg());
+            }
+        });
+    }
+}
--- app/src/main/java/com/yanlong/im/user/ui/logout/LogoutAccountActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/logout/LogoutAccountActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.jrmf360.tools.utils.ThreadUtil;
 import com.yanlong.im.R;
 import com.yanlong.im.databinding.ActivityLogoutBinding;
 
 import net.cb.cb.library.dialog.DialogCommon;
 import net.cb.cb.library.dialog.DialogCommon2;
+import net.cb.cb.library.view.ActionbarView;
 import net.cb.cb.library.view.AppActivity;
 
 /**
@@ -26,6 +28,17 @@ public class LogoutAccountActivity extends AppActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ui = DataBindingUtil.setContentView(this, R.layout.activity_logout);
+        ui.headView.getActionbar().setOnListenEvent(new ActionbarView.ListenEvent() {
+            @Override
+            public void onBack() {
+                onBackPressed();
+            }
+
+            @Override
+            public void onRight() {
+
+            }
+        });
         ui.tvConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,33 +48,45 @@ public class LogoutAccountActivity extends AppActivity {
     }
 
     private void showConfirmDialog() {
-        DialogCommon dialogConfirm = new DialogCommon(this);
-        dialogConfirm.setTitleAndSure(false, true)
-                .setLeft("取消")
-                .setRight("确定")
-                .setListener(new DialogCommon.IDialogListener() {
-                    @Override
-                    public void onSure() {
+        ThreadUtil.getInstance().runMainThread(new Runnable() {
+            @Override
+            public void run() {
+                DialogCommon dialogConfirm = new DialogCommon(LogoutAccountActivity.this);
+                dialogConfirm.setTitleAndSure(false, true)
+                        .setLeft("取消")
+                        .setRight("确定")
+                        .setContent("确定要注销账号吗？", true)
+                        .setListener(new DialogCommon.IDialogListener() {
+                            @Override
+                            public void onSure() {
 
-                    }
+                            }
 
-                    @Override
-                    public void onCancel() {
+                            @Override
+                            public void onCancel() {
 
-                    }
-                });
+                            }
+                        }).show();
+            }
+        });
+
     }
 
     private void showCommitedDialog() {
-        DialogCommon2 dialogCommited = new DialogCommon2(this);
-        dialogCommited.setButtonTxt("确定")
-                .setTitle("已经提交注册申请")
-                .setContent("工作人员将在30天内处理您的申请并删除账号下所有数据。在此期间，请不要登录常信。", false)
-                .setListener(new DialogCommon2.IDialogListener() {
-                    @Override
-                    public void onClick() {
+        ThreadUtil.getInstance().runMainThread(new Runnable() {
+            @Override
+            public void run() {
+                DialogCommon2 dialogCommited = new DialogCommon2(LogoutAccountActivity.this);
+                dialogCommited.setButtonTxt("确定")
+                        .setTitle("已经提交注册申请")
+                        .setContent("工作人员将在30天内处理您的申请并删除账号下所有数据。在此期间，请不要登录常信。", false)
+                        .setListener(new DialogCommon2.IDialogListener() {
+                            @Override
+                            public void onClick() {
 
-                    }
-                });
+                            }
+                        }).show();
+            }
+        });
     }
 }
