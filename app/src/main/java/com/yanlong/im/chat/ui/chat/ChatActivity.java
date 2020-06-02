0f2c7aac3f2c55cc4307cc435dc377d15a315f43
wl <1062678603@qq.com>
2020-06-02 17:22:37 +0800

    修复不能删除回复状态bug

--- app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
@@ -1734,6 +1734,7 @@ public class ChatActivity extends AppActivity implements IActionTagClickListener
             public void onClick() {
                 //取消回复
                 mViewModel.isReplying.setValue(false);
+                clearReply();
                 isReplying = false;
                 replayMsg = null;
             }
