783e755cee9336eee2111c56b16de2790375874b
wl <1062678603@qq.com>
2020-06-12 11:52:32 +0800

    封禁账号申诉UI

--- app/src/main/java/com/yanlong/im/user/ui/baned/BanedAccountActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/baned/BanedAccountActivity.java
@@ -2,10 +2,14 @@ package com.yanlong.im.user.ui.baned;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
 
+import com.bumptech.glide.Glide;
 import com.yanlong.im.R;
 import com.yanlong.im.databinding.ActivityBanAccountBinding;
+import com.yanlong.im.utils.GlideOptionsUtil;
 
+import net.cb.cb.library.utils.SharedPreferencesUtil;
 import net.cb.cb.library.view.ActionbarView;
 import net.cb.cb.library.view.AppActivity;
 
@@ -33,5 +37,23 @@ public class BanedAccountActivity extends AppActivity {
 
             }
         });
+        String avatar = new SharedPreferencesUtil(SharedPreferencesUtil.SPName.IMAGE_HEAD).get4Json(String.class);
+        Glide.with(this).load(avatar).apply(GlideOptionsUtil.headImageOptions()).into(ui.ivAvatar);
+
+        ui.tvConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                httpGetBalance();
+            }
+        });
+    }
+
+    private void httpGetBalance() {
+        boolean hasBalance = true;
+        if (hasBalance) {
+            go(WithdrawBalanceActivity.class);
+        } else {
+            go(WithDrawFailActivity.class);
+        }
     }
 }
---
+++ app/src/main/java/com/yanlong/im/user/ui/baned/WithDrawFailActivity.java
@@ -0,0 +1,20 @@
+package com.yanlong.im.user.ui.baned;
+
+import android.os.Bundle;
+
+import com.yanlong.im.R;
+
+import net.cb.cb.library.view.AppActivity;
+
+/**
+ * @author Liszt
+ * @date 2020/6/12
+ * Description
+ */
+public class WithDrawFailActivity extends AppActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw_fail);
+    }
+}
