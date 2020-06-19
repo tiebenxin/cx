20cb35bec5f49cdec1dd58fa672de25e7a04f92d
RaleighLuo <Raleigh.Luo@live.com>
2020-06-19 09:39:22 +0800

    更改搜索停止线程任务阻塞问题

--- app/src/main/java/com/yanlong/im/chat/task/OfflineMessage.java
+++ app/src/main/java/com/yanlong/im/chat/task/OfflineMessage.java
@@ -71,16 +71,12 @@ public class OfflineMessage extends DispatchMessage {
         repository.clear();
         //停止离线任务，
         if (executor != null && executor.getActiveCount() > 0) {
-            executor.shutdown();
-            try {   // (所有的任务都结束的时候，返回TRUE)
-                if (!executor.awaitTermination(5 * 1000, TimeUnit.MILLISECONDS)) {
-                    // 超时的时候向线程池中所有的线程发出中断(interrupted)。
-                    executor.shutdownNow();
-                }
-            } catch (InterruptedException e) {
-                // awaitTermination方法被中断的时候也中止线程池中全部的线程的执行。
-                executor.shutdownNow();
-            }
+            /**向线程池中所有的线程发出中断(interrupted)。
+             * 会尝试interrupt线程池中正在执行的线程
+             * 等待执行的线程也会被取消
+             * 但是并不能保证一定能成功的interrupt线程池中的线程。可能必须要等待所有正在执行的任务都执行完成了才能退出
+             */
+            executor.shutdownNow();
             executor = null;
         }
         mBatchSuccessMsgIds.clear();
