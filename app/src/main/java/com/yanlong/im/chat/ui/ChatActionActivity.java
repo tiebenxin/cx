2ad1b413dc7ad56ef0056782971b9572203df78c
ZhuJiaYi <zhujiayi1234@qq.com>
2020-06-28 14:44:26 +0800

    #4670 弹出的群戳窗口头像应该显示为群聊头像，显示群聊昵称 接收多次群戳一戳通知，弹框重复弹出异常问题

--- app/src/main/java/com/yanlong/im/chat/ui/ChatActionActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/ChatActionActivity.java
@@ -14,6 +14,7 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.yanlong.im.R;
 import com.yanlong.im.chat.bean.MsgAllBean;
 import com.yanlong.im.chat.bean.MsgConversionBean;
+import com.yanlong.im.chat.dao.MsgDao;
 import com.yanlong.im.chat.ui.chat.ChatActivity;
 import com.yanlong.im.utils.GlideOptionsUtil;
 import com.yanlong.im.utils.MediaBackUtil;
@@ -36,6 +37,10 @@ public class ChatActionActivity extends AppActivity {
     private LinearLayout viewYes;
     private Vibrator vibrator;
     private MsgAllBean msgAllbean;
+    private String imgUrl = "";//头像
+    private String nickName = "";//昵称
+    private MsgDao msgDao = new MsgDao();
+
 
 
     @Override
@@ -76,11 +81,19 @@ public class ChatActionActivity extends AppActivity {
             e.printStackTrace();
         }
 
-        //imgHead.setImageURI(Uri.parse("" + msgAllbean.getFrom_user().getHead()));
-        Glide.with(this).load(msgAllbean.getFrom_user().getHead())
+        //群头像/群昵称，新建群聊没有群头像和群名，需要动态获取，即自行拼凑生成
+        if(!TextUtils.isEmpty(msgAllbean.getGid())){
+            imgUrl = TextUtils.isEmpty(msgAllbean.getGroup().getAvatar()) ? msgDao.groupHeadImgGet(msgAllbean.getGid()) : msgAllbean.getGroup().getAvatar();
+            nickName = TextUtils.isEmpty(msgAllbean.getFrom_group_nickname()) ? msgDao.getGroupName(msgAllbean.getGid()) : msgAllbean.getFrom_group_nickname();
+        }else {
+            //个人头像/个人昵称
+            imgUrl = TextUtils.isEmpty(msgAllbean.getFrom_user().getHead()) ? "" : msgAllbean.getFrom_user().getHead();
+            nickName = TextUtils.isEmpty(msgAllbean.getFrom_user().getName()) ? "" : msgAllbean.getFrom_user().getName();
+        }
+        Glide.with(this).load(imgUrl)
                 .apply(GlideOptionsUtil.headImageOptions()).into(imgHead);
 
-        txtName.setText(msgAllbean.getFrom_user().getName());
+        txtName.setText(nickName);
         txtMsg.setText(msgAllbean.getStamp().getComment());
         viewNo.setOnClickListener(new View.OnClickListener() {
             @Override
