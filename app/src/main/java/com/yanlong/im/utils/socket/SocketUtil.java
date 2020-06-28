b05092bbedd0b5ec74d5f5e09509134e314339d9
wl <1062678603@qq.com>
2020-06-28 17:43:05 +0800

    修改请求日志

--- app/src/main/java/com/yanlong/im/utils/socket/SocketUtil.java
+++ app/src/main/java/com/yanlong/im/utils/socket/SocketUtil.java
@@ -131,8 +131,8 @@ public class SocketUtil {
             //保存消息和处理回执
             //在线离线消息不需要发送回执, 索引越界？？？？？
             int count = bean.getWrapMsgCount();
-            LogUtil.writeLog("--收到请求--requestId=" + bean.getRequestId() + " 条数：" + count);
             if (count > 0 && bean.getWrapMsg(0).getMsgType() != MsgBean.MessageType.ACTIVE_STAT_CHANGE) {
+                LogUtil.writeLog("--收到请求--requestId=" + bean.getRequestId() + " 条数：" + count);
                 if (count == 1) {//单条消息直接回执，多条消息待消息存成功后再回执
                     SocketUtil.getSocketUtil().sendData(SocketData.msg4ACK(bean.getRequestId(), null, bean.getMsgFrom(), false, true), null, bean.getRequestId());
                     LogUtil.writeLog("--发送回执1--requestId=" + bean.getRequestId() + " msgType:" + bean.getWrapMsg(0).getMsgType() + "--msgTypeValue=" + bean.getWrapMsg(0).getMsgTypeValue() + " msgID:" + bean.getWrapMsg(0).getMsgId());
