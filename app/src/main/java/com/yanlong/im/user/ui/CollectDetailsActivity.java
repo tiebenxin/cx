6ce9338fad3baa2d30aac5fd4fe0fc55959d6874
ZhuJiaYi <zhujiayi1234@qq.com>
2020-07-01 11:30:12 +0800

    优化->收藏列表，单独新增离线缓存逻辑，避免聊天界面消息被清空后丢失缓存导致离线收藏列表某些图片变空白 优化->收藏详情，有网的情况改为取预览图，图片不再模糊，离线照旧取缩略图缓存

--- app/src/main/java/com/yanlong/im/user/ui/CollectDetailsActivity.java
+++ app/src/main/java/com/yanlong/im/user/ui/CollectDetailsActivity.java
@@ -339,27 +339,19 @@ public class CollectDetailsActivity extends AppActivity {
                                         }
                                     }
                                 } else {
-                                    //有网情况走网络请求，无网情况拿缓存
-//                                    if(NetUtil.isNetworkConnected()){
-                                    //TODO 无需再取缓存多余处理，既然点击大图可以正常显示，说明Glide已经缓存好了，直接复用即可
-                                    Glide.with(this)
+                                    //有网情况走网络请求->显示预览图，无网情况显示Glide自带缓存缩略图
+                                    if(NetUtil.isNetworkConnected()){
+                                        Glide.with(this)
+                                                .load(gif)
+                                                .apply(GlideOptionsUtil.notDefImageOptions())
+                                                .into(ivPic);
+                                    }else {
+                                        //TODO 无需再取缓存多余处理，既然点击大图可以正常显示，说明Glide已经缓存好了，直接复用即可
+                                        Glide.with(this)
                                                 .load(thumbnail)
                                                 .apply(GlideOptionsUtil.notDefImageOptions())
                                                 .into(ivPic);
-//                                    }else {
-//                                        Bitmap localBitmap = ChatBitmapCache.getInstance().getAndGlideCache(thumbnail);
-//                                        //TODO 重置宽高，避免尺寸不对，暂行处理方法
-//                                        localBitmap = CommonUtils.imageScale(localBitmap,(int)width,(int)height);
-//                                        if (localBitmap == null) {
-//                                            Glide.with(CollectDetailsActivity.this)
-//                                                    .asBitmap()
-//                                                    .load(thumbnail)
-//                                                    .apply(GlideOptionsUtil.notDefImageOptions())
-//                                                    .into(ivPic);
-//                                        } else {
-//                                            ivPic.setImageBitmap(localBitmap);
-//                                        }
-//                                    }
+                                    }
                                 }
                                 ivPic.setOnClickListener(new View.OnClickListener() {
                                     @Override
