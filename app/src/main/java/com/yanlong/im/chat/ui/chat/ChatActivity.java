e3fecb918fdf8b1a2d81c8fdb3ddaa28900dc7c4
wl <1062678603@qq.com>
2020-08-25 16:07:17 +0800

    多选时，禁止点击跳转页面

--- app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
@@ -3764,6 +3764,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
 
     @Override
     public void clickUser(String userId, String gid) {
+        if (mAdapter != null && mAdapter.isShowCheckBox()) {
+            return;
+        }
         try {
             long user = Long.valueOf(userId);
             if (user > 0) {
@@ -3777,6 +3780,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
 
     @Override
     public void clickEnvelope(String rid) {
+        if (mAdapter != null && mAdapter.isShowCheckBox()) {
+            return;
+        }
         long tradeId = StringUtil.getLong(rid);
         if (tradeId > 0) {
             Intent intent = SingleRedPacketDetailsActivity.newIntent(ChatActivity.this, tradeId, 1);
@@ -3786,6 +3792,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
 
     @Override
     public void clickTransfer(String rid, String msgId) {
+        if (mAdapter != null && mAdapter.isShowCheckBox()) {
+            return;
+        }
         long tradeId = StringUtil.getLong(rid);
         if (tradeId > 0) {
             MsgAllBean msgAllBean = msgDao.getMsgByRid(tradeId);
@@ -3798,6 +3807,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
         if (ViewUtils.isFastDoubleClick()) {
             return;
         }
+        if (mAdapter != null && mAdapter.isShowCheckBox()) {
+            return;
+        }
         showLockDialog();
     }
 
@@ -3806,7 +3818,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
         if (ViewUtils.isFastDoubleClick()) {
             return;
         }
-
+        if (mAdapter != null && mAdapter.isShowCheckBox()) {
+            return;
+        }
         //br标签替换为换行，存之前将换行替换为br标签
         content = content.replace("<br>", "\n");
         showDraftContent(editChat.getText().toString() + content);
@@ -3821,6 +3835,9 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
 
     @Override
     public void clickAddFriend(String uid) {
+        if (mAdapter != null && mAdapter.isShowCheckBox()){
+            return;
+        }
         if (UserUtil.getUserStatus() == CoreEnum.EUserType.DISABLE) {// 封号
             ToastUtil.show(getResources().getString(R.string.user_disable_message));
             return;
