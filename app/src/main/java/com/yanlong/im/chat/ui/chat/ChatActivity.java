354790afb76f64059318bf226802947c55d7066c
ZhuJiaYi <zhujiayi1234@qq.com>
2020-09-03 17:14:42 +0800

    多选收藏弹框后，再多选转发，复用导致"取消"项丢失问题

--- app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
+++ app/src/main/java/com/yanlong/im/chat/ui/chat/ChatActivity.java
@@ -7064,6 +7064,7 @@ public class ChatActivity extends BaseTcpActivity implements IActionTagClickList
      */
     private void showForwardListDialog(List<MsgAllBean> list) {
         dialogThree = builder.setTitle("你所选的消息包含了不支持转发的类型或\n或已失效，系统已自动过滤此类型消息。")
+                .setShowLeftText(true)
                 .setRightText("继续发送")
                 .setLeftText("取消")
                 .setRightOnClickListener(v -> {
