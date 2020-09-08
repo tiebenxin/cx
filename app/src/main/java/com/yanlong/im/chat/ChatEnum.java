29e8e4b248bdea6c41ad4b72938807ec0b05bba2
ZhuJiaYi <zhujiayi1234@qq.com>
2020-09-08 11:42:59 +0800

    新增批量同意申请接口 图片编辑加入判断文件失效弹框逻辑 替换新的PB文件 入群申请，有群主/管理员确认通过后，其他管理员的该条通知刷新为"已确认"

--- app/src/main/java/com/yanlong/im/chat/ChatEnum.java
+++ app/src/main/java/com/yanlong/im/chat/ChatEnum.java
@@ -303,7 +303,7 @@ public class ChatEnum {
             ENoticeType.CHANGE_VICE_ADMINS_CANCEL, ENoticeType.FORBIDDEN_WORDS_OPEN, ENoticeType.FORBIDDEN_WORDS_CLOSE, ENoticeType.RED_ENVELOPE_RECEIVED_SELF,
             ENoticeType.FORBIDDEN_WORDS_SINGE, ENoticeType.OPEN_UP_RED_ENVELOPER, ENoticeType.SYS_ENVELOPE_RECEIVED_SELF, ENoticeType.RECEIVE_SYS_ENVELOPE, ENoticeType.SYS_ENVELOPE_RECEIVED,
             ENoticeType.GROUP_FORBID, ENoticeType.GROUP_BAN_WORDS, ENoticeType.CANCEL_CAN_EDIT, ENoticeType.FREEZE_ACCOUNT, ENoticeType.DEFAULT_USER, ENoticeType.GROUP_OTHER_REMOVE,
-            ENoticeType.SEAL_ACCOUNT, ENoticeType.FRIEND_DEACTIVATE, ENoticeType.NO_FRI_ADD_FIRST,ENoticeType.REQUEST_GROUP})
+            ENoticeType.SEAL_ACCOUNT, ENoticeType.FRIEND_DEACTIVATE, ENoticeType.NO_FRI_ADD_FIRST,ENoticeType.REQUEST_GROUP,ENoticeType.MESSAGE_PROCESSED_SYNC})
     @Retention(RetentionPolicy.SOURCE)
     public @interface ENoticeType {
         int DEFAULT = 0;//默认，简单文本
@@ -341,6 +341,7 @@ public class ChatEnum {
         int FRIEND_DEACTIVATE = 32;// 该账号已注销
         int NO_FRI_ADD_FIRST = 33;//被删好友提示先添加好友
         int REQUEST_GROUP = 34; // 请求入群
+        int MESSAGE_PROCESSED_SYNC = 35; // 消息已处理通知（各类需处理消息通用）
     }
 
     /*
