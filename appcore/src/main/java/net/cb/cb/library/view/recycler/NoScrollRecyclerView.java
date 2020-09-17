b7c8d283578d0fdd4540a0c5d364d1251faf6cb0
wl <1062678603@qq.com>
2020-09-17 15:38:16 +0800

    禁止滑动recycler

---
+++ appcore/src/main/java/net/cb/cb/library/view/recycler/NoScrollRecyclerView.java
@@ -0,0 +1,36 @@
+package net.cb.cb.library.view.recycler;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * @author Liszt
+ * @date 2020/9/17
+ * Description  禁止滑动
+ */
+public class NoScrollRecyclerView extends RecyclerView {
+    public NoScrollRecyclerView(@NonNull Context context) {
+        super(context);
+    }
+
+    public NoScrollRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NoScrollRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean canScrollHorizontally(int direction) {
+        return false;
+    }
+
+    @Override
+    public boolean canScrollVertically(int direction) {
+        return false;
+    }
+}
