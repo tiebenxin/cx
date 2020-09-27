2cd199c74b3a438a9eedddbb6676e9e6b90870a1
wl <1062678603@qq.com>
2020-09-27 14:36:23 +0800

    双向清除功能

---
+++ app/src/main/java/com/yanlong/im/user/bean/DailyReportBean.java
@@ -0,0 +1,11 @@
+package com.yanlong.im.user.bean;
+
+import net.cb.cb.library.base.BaseBean;
+
+/**
+ * @author Liszt
+ * @date 2020/9/27
+ * Description
+ */
+public class DailyReportBean extends BaseBean {
+}
